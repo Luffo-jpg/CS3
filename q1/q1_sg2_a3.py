dd9added20767fee29e949fe8c341cbbddd7226b
Luffo-jpg <ravenveniegas1005@gmail.com>
2026-08-13 09:16:53 +0800

    Add list of Zodiac signs with translations

--- q1/q1_sg2_a3.py
+++ q1/q1_sg2_a3.py
@@ -1,1 +1,15 @@
+Zodiac_Signs = [
+"i. Rat (鼠 / Shǔ)",
+"ii. Ox (牛 / Niú)",
+"iii. Tiger (虎 / Hǔ)",
+"iv. Rabbit (兔 / Tù)",
+"v. Dragon (龙 / Lóng)",
+"vi. Snake (蛇 / Shé)",
+"vii. Horse (马 / Mǎ)",
+"viii. Goat (羊 / Yáng)",
+"ix. Monkey (猴 / Hóu)",
+"x. Rooster (鸡 / Jī)",
+"xi. Dog (狗 / Gǒu)",
+"xii. Pig (猪 / Zhū)]"
+]
 
