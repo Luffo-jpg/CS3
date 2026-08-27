5266287b5caaab444f2e3ed4a6052dd808fc2b5e
Luffo-jpg <ravenveniegas1005@gmail.com>
2026-08-27 09:10:33 +0800

    Add Hero class with damage handling

--- q1/q1_sg5_a1_arayat_veniegas.py
+++ q1/q1_sg5_a1_arayat_veniegas.py
@@ -1,1 +1,13 @@
+# RPG Hero Game
+class Hero:
+    def __init__(self,name,HP):
+        self.name = name
+        self.HP = HP
+    def take_damage(self, amount):
+        self.HP = self.HP - amount
+Arthur = Hero("Arthur", 100)
+Morgana = Hero("Morgana", 100)
+Arthur.take_damage(10)
+print("Arthur's HP =", Arthur.HP)
+print("Morgana's HP =", Morgana.HP)
 
